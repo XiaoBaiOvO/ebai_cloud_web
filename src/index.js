55e3a2161c7675263603dc5c42d836414c69706a
Dasiy Huang <daisy.hmq@outlook.com>
2022-12-05 13:12:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-// import {StrictMode} from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import App from "./app";
 import store from "./redux/store";
-import {Button, ConfigProvider} from "antd";
 
 ReactDOM.render(
-    // <StrictMode>
     <Provider store={store} >
         <BrowserRouter>
-            <ConfigProvider
-                theme={{
-                    token: {
-                        colorPrimary: '#00b96b',
-                    },
-                }}
-            >
-                <Button>123</Button>
             <App/>
-            </ConfigProvider>
         </BrowserRouter>
     </Provider>,
-    // </StrictMode>,
     document.getElementById("root")
 );
